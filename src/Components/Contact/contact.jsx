8eff8c2f23bf4ab031990cda8e0cbbad53517281
Parveengill02 <parveenkaurgill02@gmail.com>
2025-06-14 19:27:55 +0530

    back

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./contact.css"
 import { useForm } from 'react-hook-form'
+import { toast, ToastContainer } from 'react-toastify';
+import axios from 'axios';
 
 function Contact() {
    const {
@@ -10,27 +12,38 @@ function Contact() {
     reset,
    }=useForm();
 
-   const onSubmit=(data)=>{
-    console.log("Form submitted successfully");
-    reset();
+   const onSubmit=async(data)=>{
+    try{
+       const res=await axios.post('http://localhost:7000/api/user/message',data);
+       console.log(res,"respose");
+       toast.success("Your message has been sent.");
+       reset();
+
+    }
+    catch(error){
+          console.log(error);
+          toast.error("!Message not sent");
+    }
+   
    }
   return (
     <div id="contact">
       <div className="Contactform">
           <div className='about'><h3 className="heading">Contact Me</h3></div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="name" placeholder='Enter your Name'
+            <input type="text" placeholder='Enter your Name'
             {...register('name',{required:"Name is required",minLength :{value:3,message:"Name must be atleast three characters"}})}/><br/>
             {errors.name && <p style={{color:"red"}}>{errors.name.message}</p>}            
              <input type="email" placeholder='Enter your Email'
             {...register('email',{required:" Email is required", pattern :{value:/^[^\s@]+@[^\s@]+\.[^\s@]+$/,message:"Email must be in the format abc@gmail.com"}})}/><br/>
             {errors.email && <p style={{color:"red"}}>{errors.email.message}</p>}
-            <input type="message" placeholder='Write your message' 
+            <input type="text" placeholder='Write your message' 
             {...register('message',{required:"Message is required" , minLength:{value:15,message:"Message must be more than 15 chracters"}})}/><br/>
             {errors.message && <p style={{color:"red"}}>{errors.message.message}</p>}
             <button type='submit'>Submit</button>
           </form>
       </div>
+      <ToastContainer/>
     </div>
   )
 }
